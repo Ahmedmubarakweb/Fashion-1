ad6843646782af6a089a532165d95cd27149e293
Ahmedmubarakweb <58855051+Ahmedmubarakweb@users.noreply.github.com>
2020-04-22 07:06:18 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,9 +15,11 @@ $('footer img').hover(
 );
 
 
-$('.move-up').hide;
 // scroll top button
 var btnscroll=$('.move-up');
+
+//to hide the btn when page is loading
+    btnscroll.hide();
 $(window).scroll(function(){
    //if height is 1000 show the icon else make it hide
     if($(this).scrollTop()>800)
